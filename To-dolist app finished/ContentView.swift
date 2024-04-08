dfb9eab5244a70836e974239f19f3d24858f43d5
Myers <mmbonda@lcs.on.ca>
2024-04-08 14:00:25 -0400

    Initial Commit

---
+++ To-dolist app finished/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To-dolist app finished
+//
+//  Created by Myers Elliott Mbonda on 2024-04-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To-dolist app finished/To_dolist_app_finishedApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_dolist_app_finishedApp.swift
+//  To-dolist app finished
+//
+//  Created by Myers Elliott Mbonda on 2024-04-08.
+//
+
+import SwiftUI
+
+@main
+struct To_dolist_app_finishedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
