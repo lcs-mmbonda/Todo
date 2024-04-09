4690a3a3d53f07892a4ed1eae23e1f2b275f3a64
Myers <mmbonda@lcs.on.ca>
2024-04-09 14:43:01 -0400

    Made the scrollable list iterate over an array.

---
+++ To-dolist app finished/Model/TdoItem.swift
@@ -0,0 +1,8 @@
+//
+//  TdoItem.swift
+//  To-dolist app finished
+//
+//  Created by Myers Elliott Mbonda on 2024-04-08.
+//
+
+import Foundation
---
+++ To-dolist app finished/Views/ItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemView.swift
+//  To-dolist app finished
+//
+//  Created by Myers Elliott Mbonda on 2024-04-08.
+//
+
+import SwiftUI
+
+struct ItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemView()
+}
